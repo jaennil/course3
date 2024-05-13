d95effc854756a84bc0a5fd17e8acf34d7a8319a
jaennil <jaennil@yandex.ru>
2024-05-13 17:22:18 +0300

    feat: home page

---
+++ src/main/java/ru/dubrovskih/first/pages/HomePage.java
@@ -0,0 +1,17 @@
+package ru.dubrovskih.first.pages;
+
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage {
+
+    @FindBy(tagName="h2")
+    private WebElement header;
+
+    public void checkHeaderIsPresent() {
+        WebElement headerElement = waitUntilElementIsVisible(header);
+        Assertions.assertEquals(headerElement.getText(), "LambdaTest Sample App");
+    }
+
+}
