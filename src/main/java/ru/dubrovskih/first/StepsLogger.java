905a2f83fd558148d2cf013838a606436ffec203
jaennil <jaennil@yandex.ru>
2024-05-16 00:22:38 +0300

    feat(lambda_test): log steps in columns, colors

--- src/main/java/ru/dubrovskih/first/StepsLogger.java
+++ src/main/java/ru/dubrovskih/first/StepsLogger.java
@@ -4,7 +4,11 @@ import io.qameta.allure.listener.StepLifecycleListener;
 import io.qameta.allure.model.StepResult;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
+import org.slf4j.MDC;
 
+import java.util.Hashtable;
+import java.util.Map;
 import java.util.Objects;
 
 public class StepsLogger implements StepLifecycleListener {
@@ -13,7 +17,15 @@ public class StepsLogger implements StepLifecycleListener {
 
     private void logStep(StepResult stepResult) {
         if (!Objects.equals(stepResult.getName(), "step")) {
+            ThreadContext.put("name", stepResult.getName());
+            ThreadContext.put("stage", stepResult.getStage().toString());
+            if (stepResult.getStatus() == null) {
+                ThreadContext.put("status", "null");
+            } else {
+                ThreadContext.put("status", stepResult.getStatus().toString());
+            }
             logger.info("{} {} {}", stepResult.getName(), stepResult.getStage(), stepResult.getStatus());
+            ThreadContext.clearAll();
         }
     }
 
